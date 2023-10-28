b97bc1251717d04753ed7b3b23e9cdfd4368a56c
Snippy <acc.snippy@tutanota.com>
2023-10-28 11:34:34 +0200

    Added NavBar

--- main.dart
+++ main.dart
@@ -41,71 +41,105 @@ class MyAppState extends ChangeNotifier {
     } else {
       favorites.add(current);
     }
+    notifyListeners();
   }
 
 }
 
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
 
+var selectedIndex = 0;
 
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch(selectedIndex){
+      case 0:  page = GeneratorPage();
+      case 1:  page = Placeholder();
+      default: throw UnimplementedError('no wiget for $selectedIndex');
+    }
+    
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });                
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-
-
-
-
-
-
-
-
-class MyHomePage extends StatelessWidget {
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var wordPair = appState.current;
-    var theme = Theme.of(context);
 
-    return Scaffold(
-      appBar: AppBar(title: const Text('Header'),backgroundColor: theme.colorScheme.primary, ),
-      body: Center(
-        child: Column(
+    IconData icon;
+    if (appState.favorites.contains(wordPair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+            child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [            
               BigCard(wordPair: wordPair),
               SizedBox(height: 30),
               Row(                
                 mainAxisSize: MainAxisSize.min,
-                
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      print('butten Pressed');
-                      appState.getNext();
-                      print(appState.current);
-                    }, 
-                    child: Text('next')
+                  ElevatedButton.icon(
+                    onPressed: () {appState.toggleFavorites(); },
+                    icon: Icon(icon),
+                    label: Text('Like'),
                     ),
+                    SizedBox(width: 30,),
                     ElevatedButton(
                       onPressed: () {
-                        appState.toggleFavorites();
-                        print(appState.favorites);
+                        appState.getNext();
                       } 
-                    , child: Text('Like'),
+                    , child: Text('Next'),
                     
                     )      
                 ],
               )
             ],
-          ),
-      ),      
-    );
+          ),);  
   }
 }
-
-
-
-
-
-
-
 
 class BigCard extends StatelessWidget {
   const BigCard({
